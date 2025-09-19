9a494951e0dacaf388f9b28505ceb5fa4a159cf3
Anupam Semwal <140145702+XENDERMARIA@users.noreply.github.com>
2025-09-19 15:53:13 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 // Get private key from environment
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 
 module.exports = {
   solidity: {
